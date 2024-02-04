f4572857d21469abe169ba88e3b2c24a52d07bf7
vitostamatti <vitostamatti@gmail.com>
2024-02-04 14:32:09 +0100

    fix test

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,12 +1,5 @@
-def test_import_events():
-    from pydes.events import Simulator
-
-    sim = Simulator()
-    assert isinstance(sim, Simulator)
-
-
-def test_import_process():
-    from pydes.process import Simulator
+def test_todo():
+    from pydes import Simulator
 
     sim = Simulator()
     assert isinstance(sim, Simulator)
